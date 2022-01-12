2fa3c62a98b88e418fadf9e577bf2dfd0c738737
vchungtx <vchungtx@gmail.com>
2022-01-12 18:30:12 +0700

    Update HomeController.java

--- chatbot/src/main/java/org/muahangsieure/chatbot/fbmessenger/HomeController.java
+++ chatbot/src/main/java/org/muahangsieure/chatbot/fbmessenger/HomeController.java
@@ -17,7 +17,7 @@ public class HomeController {
 
     @GetMapping("/webhook")
     public ResponseEntity<?> getWebhook(@RequestParam("hub.mode") String mode, @RequestParam("hub.verify_token") String verify_token, @RequestParam("hub.challenge") String challenge) {
-        if (mode.equals("subscribe'") && verify_token.equals("EAADO4cZA6ZAXkBAKYhfKZAYW1CyIcjbqYack3KePdoZCEZAZCXZCkjibKkR84S83YUIp0nXHKEpXJuNw4h4RMz6tO7ClC0p0IEahY8ZAoZA1TTDsVNZCs9qp44JuISx2Le6naEdtA846oVRnC3PhaaWBTXuUKs5EI4rGuSiMU519jJOS0QNdJWZCQ2d")){
+        if (mode.equals("subscribe") && verify_token.equals("EAADO4cZA6ZAXkBAKYhfKZAYW1CyIcjbqYack3KePdoZCEZAZCXZCkjibKkR84S83YUIp0nXHKEpXJuNw4h4RMz6tO7ClC0p0IEahY8ZAoZA1TTDsVNZCs9qp44JuISx2Le6naEdtA846oVRnC3PhaaWBTXuUKs5EI4rGuSiMU519jJOS0QNdJWZCQ2d")){
             return ResponseEntity.status(200).body(challenge);
         }else{
             return ResponseEntity.status(403).body("");
